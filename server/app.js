a42858b1aaec993f3480ad28bb2cc7c328852518
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-05-10 00:06:47 +0530

    app init

---
+++ server/app.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+import { config } from 'dotenv'
+
+config()
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL
+}))
+
+app.get("/", (req, res)=> {
+    res.send("working fine")
+})
+
+export default app
