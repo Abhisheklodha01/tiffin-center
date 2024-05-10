927149be440f00945b26b8cf5d9404997d4f84dc
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-05-10 15:46:44 +0530

    connecting to database

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import cors from 'cors'
 import { config } from 'dotenv'
+import connectDB from './db/index.js'
 
 config()
 
+connectDB()
 const app = express()
 
 app.use(cors({
--- server/db/index.js
+++ server/db/index.js
@@ -1,1 +1,17 @@
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+ const connectDB = async () => {
+    try {
+       const connectionInstance = await mongoose.connect(process.env.MONGO_URI)
+        console.log("Database connected");
+        console.log(`DB Host: ${connectionInstance.connection.host} `);
+    } catch (error) {
+        console.log("Database connection failed");
+        console.log(error);
+    }
+}
+
+export default connectDB
