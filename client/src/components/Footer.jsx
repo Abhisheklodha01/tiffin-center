8ad75f317ab0613ed2cdff1b6f1d690a4570c05d
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-05-12 16:01:35 +0530

    creating basic footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-gray-900 text-gray-300 py-12">
+      <div
+        className="max-w-6xl mx-auto grid grid-cols-1
+       sm:grid-cols-2 lg:grid-cols-4 gap-8 px-4 sm:px-6 lg:px-8"
+      >
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">Quick Links</h2>
+          <ul>
+            <li>
+              <a
+                href="#"
+                className="hover:text-white transition-colors duration-300"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="hover:text-white transition-colors duration-300"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="hover:text-white transition-colors duration-300"
+              >
+                Foods
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="hover:text-white transition-colors duration-300"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">Follow Us</h2>
+          <div className="flex space-x-4">
+            <a
+              href="#"
+              className="hover:text-white transition-colors duration-300"
+            >
+              Facebook
+            </a>
+            <a
+              href="#"
+              className="hover:text-white transition-colors duration-300"
+            >
+              Twitter
+            </a>
+            <a
+              href="#"
+              className="hover:text-white transition-colors duration-300"
+            >
+              Instagram
+            </a>
+          </div>
+        </div>
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">Contact Us</h2>
+          <p>Guna MP, India</p>
+          <p>Guna 473226</p>
+          <p>Email: info@tifin-center.com</p>
+          <p>Phone: (123) 456-7890</p>
+        </div>
+      </div>
+      <p className="text-center text-lg pt-8">
+        © 2024 Tifin Center. All rights reserved.
+      </p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
