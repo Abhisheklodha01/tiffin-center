c0d452e2ce53b31ff9656534f48501923f5e9520
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-05-17 11:32:11 +0530

    adjusting height

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import HomeImage from "../assets/HomeImage.jpg";
-import Home1 from "../assets/Home1.jpg";
 import SliderImage1 from "../assets/SliderImage1.jpg";
 
 const Home = () => {
   return (
-    <div className="">
-      <div className="carousel w-full h-1/2 ">
+    <div className="min-h-screen">
+      <div className="carousel w-full">
         <div id="slide1" className="carousel-item relative w-full">
           <img
             src={SliderImage1}
-            className="w-full h-1/2"
+            className="w-full h-screen md:h-[700px]"
           />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide4" className="btn btn-circle">
