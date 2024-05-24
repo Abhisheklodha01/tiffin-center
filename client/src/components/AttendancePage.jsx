8bc0a462912845abf6e549291cf76c9f1d85920b
Abhishek Lodha <abhisheklodha512@gmail.com>
2024-05-24 23:14:31 +0530

    completing User Attandance page

--- client/src/components/AttendancePage.jsx
+++ client/src/components/AttendancePage.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { toast } from "react-hot-toast";
 import { server } from "../utils/constants.js";
+import { Context } from "../main.jsx";
 
 const AttendancePage = () => {
+  const { user } = useContext(Context);
   const token = localStorage.getItem("token");
   const [userAttendance, setUserAttendance] = useState([]);
 
@@ -21,22 +23,42 @@ const AttendancePage = () => {
     };
     fetchUserAttendance();
   }, []);
+  const formatDateString = (dateString) => {
+    const date = new Date(dateString);
+    const day = String(date.getUTCDate()).padStart(2, "0");
+    const month = String(date.getUTCMonth() + 1).padStart(2, "0");
+    const year = date.getUTCFullYear();
+    return `${day}-${month}-${year}`;
+  };
   return (
     <div
-      className="min-h-screen bg-gray-700 flex
-     justify-center items-center flex-col pt-32"
+      className="min-h-screen bg-[#15616d] flex
+     justify-center items-center flex-col pt-32 text-white"
     >
-      <div className="flex flex-row gap-20 text-2xl font-bold mb-5">
-        <h1 className="mr-16">Status</h1>
-        <h1 className="mr-10">Date</h1>
-        <h1 className="ml-10">Shift</h1> 
+      <div className="flex flex-col md:flex-row gap-5 md:gap-10 mb-8 text-lg">
+        <h1 className="text-xl">
+          Name: <span>{user.fullName}</span>
+        </h1>
+        <h1 className="text-xl">
+          Email: <span>{user.email}</span>
+        </h1>
+      </div>
+      <div className="flex flex-row gap-16 md:gap-20 text-2xl font-bold mb-5">
+        <h1 className="md:mr-10">Status</h1>
+        <h1 className="md:mr-10">Date</h1>
+        <h1 className="md:mr-4 ml-4">Shift</h1>
       </div>
       {userAttendance.map(({ status, date }) => (
         <div>
-          <ul className="flex gap-12 text-xl pt-2 pb-5">
-            <li className ={status == 'present' ? "text-green-600" : "text-amber-600"}>
-                {status.toUpperCase()}</li>
-            <li>{date}</li>
+          <ul className="flex gap-8 md:gap-12 text-xl pt-2 pb-5 m-2">
+            <li
+              className={
+                status == "present" ? "text-green-500" : "text-amber-500"
+              }
+            >
+              {status.toUpperCase()}
+            </li>
+            <li className="md:mr-12 md:ml-12">{formatDateString(date)}</li>
             <li>MORNING</li>
           </ul>
         </div>
